9a9a60b23e8261265a81fc557080be49f07ceec6
AbdElRahman Rizq <47121655+AbdElRahman-Rizq@users.noreply.github.com>
2024-03-30 17:58:46 +0200

    Initilization Prisma

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @Post("signup")
+  signup(){
+    return this.authService.signUp()
+  }
+  @Post("signIn")
+  signIn(){
+    return "signIn"
+  }
+  @Post("signOut")
+  signOut(){
+    return "signOut"
+  }
 }
