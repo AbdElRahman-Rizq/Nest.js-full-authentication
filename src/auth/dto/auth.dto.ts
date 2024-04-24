11ce9e6f07f96e4fb6353515b73f68ccbcdb34d8
AbdElRahman Rizq <47121655+AbdElRahman-Rizq@users.noreply.github.com>
2024-04-24 11:23:41 +0200

    Change the example of schema

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -5,12 +5,12 @@ export class AuthDto {
   @IsNotEmpty()
   @IsString()
   @IsEmail()
-  @ApiProperty({ example: 'john_doe@example.com', description: 'Email of the user' })
+  @ApiProperty({ example: 'Rizq@example.com', description: 'Email of the user' })
   public email: string;
 
   @IsNotEmpty()
   @IsString()
-  @Length(5, 20, { message: 'Password should be between 5 to 20 characters' })
+  @Length(5, 20, { message: 'Password should be between 6 to 15 characters' })
   @ApiProperty({ example: 'password123', description: 'Password of the user' })
   public password: string;
 }
