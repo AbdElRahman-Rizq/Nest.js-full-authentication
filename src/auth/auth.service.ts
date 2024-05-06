0aba350a9c14f0f344d058d481dea1a382374534
AbdElRahman Rizq <47121655+AbdElRahman-Rizq@users.noreply.github.com>
2024-05-07 00:01:37 +0300

    change the path

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,19 +2,20 @@ import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/com
 import { AuthDto } from './dto/auth.dto';
 import { PrismaService } from '../../prisma/prisma.service';
 import { hashingPassword, comparingPassword } from './authHelper';
-
-import {JwtService} from "@nestjs/jwt"
+import { JwtService } from '@nestjs/jwt';
 import { Jwt_Secret } from '../utils/constant';
 import { Request, Response } from 'express';
+
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService,private jwt:JwtService) {}
+  constructor(private readonly prisma: PrismaService, private jwt: JwtService) {}
+
   // Sign up Function
   async signUp(dto: AuthDto) {
     const { email, password } = dto;
     const foundUser = await this.prisma.user.findUnique({ where: { email } });
     if (foundUser) {
-      throw new BadRequestException('Email is already exists');
+      throw new BadRequestException('Email already exists');
     }
     const hashedPassword = await hashingPassword(password);
     await this.prisma.user.create({
@@ -27,36 +28,38 @@ export class AuthService {
   }
 
   // Sign In Function
-  async signIn(dto: AuthDto,req:Request,res:Response) {
+  async signIn(dto: AuthDto, req: Request, res: Response) {
     const { email, password } = dto;
     // Check Email
     const foundUser = await this.prisma.user.findUnique({ where: { email } });
     if (!foundUser) {
-      throw new BadRequestException('Wrong Email or Passwprd');
+      throw new BadRequestException('Wrong email or password');
     }
     // Check Password
-    const isMAtch = await comparingPassword({
-        password,
-        hashed: foundUser.hashedPassword,
+    const isMatch = await comparingPassword({
+      password,
+      hashed: foundUser.hashedPassword,
     });
-    if (!isMAtch) {
-      throw new BadRequestException('Wrong Email or Passwprd');
+    if (!isMatch) {
+      throw new BadRequestException('Wrong email or password');
     }
-    const token=await this.signToken({id:foundUser.id,email:foundUser.email});
+    const token = await this.signToken({ id: foundUser.id, email: foundUser.email });
     if (!token) {
       throw new ForbiddenException();
     }
-    res.cookie("token",token)
-    return res.send({message:"Logged in Successfully"});
+    res.cookie('token', token);
+    return res.send({ message: 'Logged in successfully' });
   }
-//   sign out 
-async signOut(req:Request,res:Response){
-res.clearCookie("token")
-return "Logged out"
-}
+
+  // Sign out function
+  async signOut(req: Request, res: Response) {
+    res.clearCookie('token');
+    return 'Logged out';
+  }
+
   // Function for creating token when login
   async signToken(args: { id: string; email: string }) {
-    const payload=args;
-   return await  this.jwt.signAsync(payload,{secret:Jwt_Secret})
-}
+    const payload = args;
+    return await this.jwt.signAsync(payload, { secret: Jwt_Secret });
+  }
 }
